4f960c11603db9fa8c639771cb9a8ab7590b0a5d
soejimareona <soejima.reona@m.alhinc.jp>
2020-09-28 13:47:01 +0900

    テストケースの追加・不要処理の削除・修正

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -3,29 +3,29 @@ package fizzbuzz;
 public class FizzBuzz {
 
 	public String response(int num) {
+
 		StringBuilder result = new StringBuilder();
 
+		//不正な引数をメソッドに渡した場合のエラー
 		if (num < 1) {
-			throw new IndexOutOfBoundsException();
+			throw new IllegalArgumentException();
 		}
 
-		if (num % 3 == 0 && num % 5 == 0) {
-			System.out.println("FizzBuzz");
-			result.append("FizzBuzz");
-		} else if (num % 3 == 0) {
-			System.out.println("Fizz");
+		//FizzBuzzの条件
+		//3と5の倍数ならFizzBuzzと連結して出力
+		if (num % 3 == 0) {
 			result.append("Fizz");
-		} else if (num % 5 == 0) {
-			System.out.println("Buzz");
-			result.append("Buzz");
-		} else {
-			System.out.println(num);
 		}
 
+		if (num % 5 == 0) {
+			result.append("Buzz");
+		}
+
+		//3と5の倍数でない場合
 		if (result.length() == 0) {
 			result.append(String.valueOf(num));
 		}
-
 		return result.toString();
 	}
+
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -21,6 +21,26 @@ public class FizzBuzzTest {
 		public void 引数に1を与えたら1を返す() {
 			assertEquals("1", fizzbuzz.response(1));
 		}
+
+		@Test
+		public void 引数に22を与えたら22を返す() {
+			assertEquals("22", fizzbuzz.response(22));
+		}
+
+		@Test
+		public void 引数に508を与えたら508を返す() {
+			assertEquals("508", fizzbuzz.response(508));
+		}
+
+		@Test
+		public void 引数に8888を与えたら8888を返す() {
+			assertEquals("8888", fizzbuzz.response(8888));
+		}
+
+		@Test
+		public void 引数に70001を与えたら70001を返す() {
+			assertEquals("70001", fizzbuzz.response(70001));
+		}
 	}
 
 	public static class 引数が3のみの倍数 {
@@ -29,6 +49,26 @@ public class FizzBuzzTest {
 		@Test
 		public void 引数3を与えたらFizzを返す() {
 			assertEquals("Fizz", fizzbuzz.response(3));
+		}
+
+		@Test
+		public void 引数33を与えたらFizzを返す() {
+			assertEquals("Fizz", fizzbuzz.response(33));
+		}
+
+		@Test
+		public void 引数999を与えたらFizzを返す() {
+			assertEquals("Fizz", fizzbuzz.response(999));
+		}
+
+		@Test
+		public void 引数6666を与えたらFizzを返す() {
+			assertEquals("Fizz", fizzbuzz.response(6666));
+		}
+
+		@Test
+		public void 引数99999を与えたらFizzを返す() {
+			assertEquals("Fizz", fizzbuzz.response(99999));
 		}
 	}
 
@@ -39,6 +79,27 @@ public class FizzBuzzTest {
 		public void 引数5を与えたらBuzzを返す() {
 			assertEquals("Buzz", fizzbuzz.response(5));
 		}
+
+		@Test
+		public void 引数20を与えたらBuzzを返す() {
+			assertEquals("Buzz", fizzbuzz.response(20));
+		}
+
+		@Test
+		public void 引数500を与えたらBuzzを返す() {
+			assertEquals("Buzz", fizzbuzz.response(500));
+		}
+
+		@Test
+		public void 引数5555を与えたらBuzzを返す() {
+			assertEquals("Buzz", fizzbuzz.response(5555));
+		}
+
+		@Test
+		public void 引数70000を与えたらBuzzを返す() {
+			assertEquals("Buzz", fizzbuzz.response(70000));
+		}
+
 	}
 
 	public static class 引数が3と5の倍数 {
@@ -48,12 +109,32 @@ public class FizzBuzzTest {
 		public void 引数15を与えたらFizzBuzzを返す() {
 			assertEquals("FizzBuzz", fizzbuzz.response(15));
 		}
+
+		@Test
+		public void 引数60を与えたらFizzBuzzを返す() {
+			assertEquals("FizzBuzz", fizzbuzz.response(60));
+		}
+
+		@Test
+		public void 引数150を与えたらFizzBuzzを返す() {
+			assertEquals("FizzBuzz", fizzbuzz.response(150));
+		}
+
+		@Test
+		public void 引数9000を与えたらFizzBuzzを返す() {
+			assertEquals("FizzBuzz", fizzbuzz.response(9000));
+		}
+
+		@Test
+		public void 引数60000を与えたらFizzBuzzを返す() {
+			assertEquals("FizzBuzz", fizzbuzz.response(60000));
+		}
 	}
 
 	public static class 引数が無効境界値 {
 		FizzBuzz fizzbuzz = new FizzBuzz();
 
-		@Test(expected = IndexOutOfBoundsException.class)
+		@Test(expected = IllegalArgumentException.class)
 		public void 引数0を与えたらエラーとなる() {
 			fizzbuzz.response(0);
 		}
